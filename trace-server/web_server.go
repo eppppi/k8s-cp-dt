c261b7e91e641b2b2cd93918070d1d5097f2f043
eppppi <12tomo02yuki@gmail.com>
2023-12-28 06:04:17 +0000

    extract html text to a file

--- trace-server/web_server.go
+++ trace-server/web_server.go
@@ -11,21 +11,6 @@ import (
 )
 
 func rootHandler(w http.ResponseWriter, _ *http.Request) {
-	// tmp := `
-	// <!DOCTYPE html>
-	// <html>
-	// <head>
-	// 	<meta charset="UTF-8">
-	// 	<title>trace-server</title>
-	// 	</head>
-	// <body>
-	// 	<h2>This server serves the following endpoints.</h2>
-	// 	{{ range . }}
-	// 		<p><a href="{{- . }}">{{- . }}</a></p>
-	// 	{{ end }}
-	// </body>
-	// </html>`
-	// t := template.Must(template.New("root").Parse(tmp))
 	t, err := template.ParseFiles("root/index.html")
 	if err != nil {
 		log.Fatal(err)
@@ -42,23 +27,10 @@ func rootHandler(w http.ResponseWriter, _ *http.Request) {
 }
 
 func getAllMergelogsHandler(w http.ResponseWriter, _ *http.Request) {
-	tmp := `
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>trace-server</title>
-		</head>
-	<body>
-		<h2>All Mergelogs</h2>
-		<ul>
-		{{ range . }}
-			<li>{{ .}}</li>
-		{{ end }}
-		</ul>
-	</body>
-	</html>`
-	t := template.Must(template.New("getAllMergelogs").Parse(tmp))
+	t, err := template.ParseFiles("root/template/getAllMergelogs.html")
+	if err != nil {
+		log.Fatal(err)
+	}
 	t.Execute(w, mergelogList.getAll())
 }
 
@@ -71,22 +43,6 @@ func getAllMergelogsImageHandler(w http.ResponseWriter, _ *http.Request) {
 		log.Fatal(err)
 	}
 
-	// return the image
-	// tmp := `
-	// <!DOCTYPE html>
-	// <html>
-	// <head>
-	// 	<meta charset="UTF-8">
-	// 	<title>trace-server</title>
-	// 	</head>
-	// <body>
-	// 	<h2>All Mergelogs Image</h2>
-	// 	<img src="https://www.pokemon.co.jp/img/logo.png" alt="">
-	// 	<img src="{{ . }}" alt="graph image">
-	// 	<img src="../generated-img/graph-2667192793.png" alt="graph image2">
-	// </body>
-	// </html>`
-	// t := template.Must(template.New("getAllMergelogsImage").Parse(tmp))
 	t, err := template.ParseFiles("root/template/getAllMergelogsImage.html")
 	if err != nil {
 		log.Fatal(err)
